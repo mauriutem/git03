1cfe139a1e1fb44ea8f4ebf252f243dfadc2879e
mauricio araya <mauri_araya@outlook.com>
2017-08-18 14:27:51 -0300

    Agregar archivos de proyecto.

---
+++ git03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(git03.Startup))]
+namespace git03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
